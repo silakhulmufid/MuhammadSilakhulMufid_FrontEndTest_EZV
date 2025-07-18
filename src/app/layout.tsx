fae31fe13c8ce1c7d62c7f0742f3b9e1b389e3e3
silakhulmufid <silakhulmufid@gmail.com>
2025-07-18 09:34:39 +0700

    feat(analytics): add vercel analytics package and component
    Integrate Vercel Analytics to track page views and user interactions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/providers";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Providers>{children}</Providers>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
